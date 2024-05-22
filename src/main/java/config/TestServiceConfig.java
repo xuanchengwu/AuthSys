b29ff404575ffb1a2c1c8f4c933bba2ae5861c37
xuanchengwu <wxc1121@gmail.com>
2024-05-22 01:13:34 -0400

    Finished

---
+++ src/main/java/config/TestServiceConfig.java
@@ -0,0 +1,19 @@
+package config;
+
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class TestServiceConfig {
+
+
+//
+//    @Bean
+//    public UserServices userService() {
+//        return new UserServices(); // Provide a real implementation
+//    }
+
+//    @Bean
+//    public AdminServices adminService() {
+//        return new AdminServices(); // Provide a real implementation
+//    }
+}
